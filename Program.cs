5f734337eb66e666d78c323e797da258e2916aea
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-24 23:24:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swaping_the_char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String a = "abcdefg";//swapping the string of char
+            char[] c = a.ToCharArray();
+            char temp = c[0];
+            c[0] = c[1];
+            
+            c[1] = temp;
+            char temp1 = c[4];
+            c[4] = c[5];
+            c[5] = temp1;
+
+            {
+                Console.WriteLine(c);
+            }
+
+
+        }
+    }
+}
